03607462552cf44e88187956510b3400b1d67909
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-10 22:43:50 +0530

    🚑 Implement doctor profile update route with email parameter in doctorRouter

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getAllDoctors, getDoctor } from "../controllers/doctorController.js";
+import {
+  getAllDoctors,
+  getDoctor,
+  updateDoctor,
+} from "../controllers/doctorController.js";
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/", getAllDoctors);
 doctorRouter.get("/getdoctor", getDoctor);
+doctorRouter.put("/:email", updateDoctor);
 
 export default doctorRouter;
