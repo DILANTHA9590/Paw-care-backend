f6917cc6f19dc88b25241230123e4c46f3cd2279
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-15 23:27:13 +0530

    Add route to get all pet profiles

--- routes/petRoute.js
+++ routes/petRoute.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   createPet,
   DeletePetdetails,
+  getAllPets,
   updatePetdetails,
-} from "../controllers/petController";
+} from "../controllers/petController.js";
 
 const petRouter = express.Router();
 
 petRouter.post("/", createPet);
 petRouter.put("/:petId", updatePetdetails);
 petRouter.delete("/:petId", DeletePetdetails);
+petRouter.get("/", getAllPets);
 
 export default petRouter;
