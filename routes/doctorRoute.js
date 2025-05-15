373d92206d7da4e81ac15840df97d4d02f5541e6
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-15 12:01:19 +0530

    create route for delete doctor function🌟

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  deleteDoctor,
   getAllDoctors,
   getDoctor,
   updateDoctor,
@@ -10,5 +11,6 @@ const doctorRouter = express.Router();
 doctorRouter.get("/", getAllDoctors);
 doctorRouter.get("/getdoctor", getDoctor);
 doctorRouter.put("/:email", updateDoctor);
+doctorRouter.delete("/:doctorId", deleteDoctor);
 
 export default doctorRouter;
