7da66d7ebbde50d1d48be046e4ec3972a1a3ef0f
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-16 12:35:07 +0530

    🛣️ Add route to get logged-in user's pets (/mypets)

--- routes/petRoute.js
+++ routes/petRoute.js
@@ -3,6 +3,7 @@ import {
   createPet,
   DeletePetdetails,
   getAllPets,
+  getMyPets,
   updatePetdetails,
 } from "../controllers/petController.js";
 
@@ -12,5 +13,6 @@ petRouter.post("/", createPet);
 petRouter.put("/:petId", updatePetdetails);
 petRouter.delete("/:petId", DeletePetdetails);
 petRouter.get("/", getAllPets);
+petRouter.get("/mypets", getMyPets);
 
 export default petRouter;
