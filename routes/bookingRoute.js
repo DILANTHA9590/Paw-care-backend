11e939a35f174638b01273526c503cdca68b9e60
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-19 22:18:51 +0530

    Add route for admin to update booking by bookingId✅

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -3,6 +3,7 @@ import {
   createBooking,
   deleteBooking,
   getAllBooking,
+  updateBooking,
 } from "../controllers/bookingController.js";
 
 const bookingRouter = express.Router();
@@ -10,5 +11,6 @@ const bookingRouter = express.Router();
 bookingRouter.post("/", createBooking);
 bookingRouter.get("/", getAllBooking);
 bookingRouter.delete("/:bookingId", deleteBooking);
+bookingRouter.put("/:bookingId", updateBooking);
 
 export default bookingRouter;
