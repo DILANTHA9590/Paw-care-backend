f7be6fe154129e01cd34b5e8de6a93f725bc2115
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-23 20:50:43 +0530

    create route for get all products✅🌟

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createProduct,
   deleteProduct,
+  getAllProduct,
 } from "../controllers/productController.js";
 
 const productRouter = express.Router();
@@ -9,5 +10,6 @@ const productRouter = express.Router();
 productRouter.post("/", createProduct);
 productRouter.delete("/:productId", deleteProduct);
 productRouter.put("/:productId", deleteProduct);
+productRouter.get("/getproduct", getAllProduct);
 
 export default productRouter;
