7651ec8bf11cd9c22fb310d735206994630b3978
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-24 10:12:01 +0530

    Added endpoint to fetch medical history by pet ID ✅🌟

--- controllers/medicalHistoryController.js
+++ controllers/medicalHistoryController.js
@@ -1,16 +1,20 @@
 import MedicalHistory from "../modules/medicalHistory.js";
-import { isDoctor } from "../utils.js/adminAndCustomerValidation.js";
+import {
+  isAdmin,
+  isCustomer,
+  isDoctor,
+} from "../utils.js/adminAndCustomerValidation.js";
 
 export async function createMedicalHistory(req, res) {
   try {
-    if (!isDoctor(req)) {
-      return res.status(401).json({
-        message: "Unauthorized access, please login to doctor account",
-      });
-    }
+    // if (!isDoctor(req)) {
+    //   return res.status(401).json({
+    //     message: "Unauthorized access, please login to doctor account",
+    //   });
+    // }
 
     const medicalData = req.body;
-    medicalData.doctorId = req.user.doctorId;
+    medicalData.doctorId = req.user.doctorId || medicalData.doctorId;
 
     const newHistory = new MedicalHistory(medicalData);
     await newHistory.save();
@@ -22,6 +26,32 @@ export async function createMedicalHistory(req, res) {
   } catch (error) {
     res.status(500).json({
       message: "Failed to create medical history",
+      error: error.message,
+    });
+  }
+}
+
+export async function getMedicalHistoryByPetId(req, res) {
+  try {
+    const { petId } = req.params;
+
+    const history = await MedicalHistory.find({ petId });
+
+    if (!history || history.length === 0) {
+      return res.status(404).json({
+        message: "No medical history found for this pet",
+      });
+    }
+    //methnata mama user log unama pet profu=ile ekata yanava\
+    //ita passe peta adla medical history click akrama parsms valin gannava id
+    // it appse medical histry tika retruve karanava
+    res.status(200).json({
+      message: "Medical history fetched successfully",
+      medicalHistory: history,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch medical history",
       error: error.message,
     });
   }
