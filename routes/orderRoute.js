aae82d35b46014c3f1c04a3cd61ad23032d79bcf
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-26 22:47:38 +0530

    🛠️ Add test route to orderRouter for tryTest function✅✅

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,6 +3,7 @@ import {
   createOrder,
   getOrdersByAdmin,
   getOrdersByCustomerId,
+  tryTest,
 } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
@@ -10,5 +11,6 @@ const orderRouter = express.Router();
 orderRouter.post("/", createOrder);
 orderRouter.get("/", getOrdersByCustomerId);
 orderRouter.get("/admin", getOrdersByAdmin);
+orderRouter.get("/test", tryTest);
 
 export default orderRouter;
