0cad231e3dbdf0b25090aa24daa1b2348a6654ad
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-05-26 12:59:59 +0530

    add route to get orders by customer ID🌟✅

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController.js";
+import {
+  createOrder,
+  getOrdersByCustomerId,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/", createOrder);
+orderRouter.get("/", getOrdersByCustomerId);
 
 export default orderRouter;
