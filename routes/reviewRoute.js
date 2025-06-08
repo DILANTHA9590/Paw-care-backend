29aa871faf659620c25a959895fb350987c78b5f
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-08 21:06:57 +0530

    🔧 Add route to fetch all reviews using getAllReviews controller

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   createReview,
+  getAllReviews,
   getReviewsByDoctorId,
 } from "../controllers/reviewController.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.post("/", createReview);
+reviewRouter.get("/", getAllReviews);
 // routes/reviewRouter.js
 reviewRouter.get("/:doctorId", getReviewsByDoctorId);
 
