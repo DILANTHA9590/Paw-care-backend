89050c329ef332e173fe90f747083f86018a200e
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-06-08 21:05:05 +0530

    📥 Added paginated review fetching with default query params, sorting by newest, total count, and error handling.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -8,11 +8,11 @@ import { checkRequredField } from "../utils.js/checkRequiredField.js";
 
 export async function createReview(req, res) {
   try {
-    // if (!isCustomer(req) && !isAdmin(req)) {
-    //   return res.status(401).json({
-    //     message: "Please login first to add a review",
-    //   });
-    // }
+    if (!isCustomer(req) && !isAdmin(req)) {
+      return res.status(401).json({
+        message: "Please login first to add a review",
+      });
+    }
 
     const reviewsData = req.body;
     const requiredFields = ["doctorId", "customerId", "rating", "comment"];
@@ -66,5 +66,34 @@ export async function getReviewsByDoctorId(req, res) {
   } catch (error) {
     console.error("Error fetching reviews:", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getAllReviews(req, res) {
+  console.log("inside this");
+
+  try {
+    const { page = 1, limit = 10 } = req.query; // default page 1, limit 10
+    console.log(page);
+    console.log(limit);
+
+    const skip = (page - 1) * limit;
+
+    const reviewData = await Rewies.find()
+      .sort({ reviewDate: -1 }) // newest first
+      .skip(skip)
+      .limit(limit);
+
+    const totalReviews = await Rewies.countDocuments();
+
+    res.status(200).json({
+      reviews: reviewData,
+      total: totalReviews,
+      currentPage: page,
+      totalPages: Math.ceil(totalReviews / limit),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong." });
   }
 }
