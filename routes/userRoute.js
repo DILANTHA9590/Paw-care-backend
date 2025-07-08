dd5813e6b2d8b9e294de78e76cf9c46774aeab33
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-08 16:05:15 +0530

    add GET /user-data for customer profile and POST /add-image for admin image upload🌟

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
+  AddUserImage,
   createUser,
   deleteSelectedUsers,
   deleteUser,
+  getDetailsForUserProfile,
   getUsers,
   loginUser,
   UpdateUser,
@@ -12,6 +14,8 @@ const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/add-image", AddUserImage);
+userRouter.get("/user-data", getDetailsForUserProfile);
 userRouter.delete("/selectusers", deleteSelectedUsers);
 userRouter.put("/:email", UpdateUser);
 userRouter.get("/", getUsers);
