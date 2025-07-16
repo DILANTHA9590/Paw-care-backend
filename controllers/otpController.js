b5d4dafedbc802c19b2706b6c012276c47eb99a8
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-16 20:21:02 +0530

    improve VerifyEmail to check OTP expiry and ensure user exists before verifying✅

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -5,7 +5,14 @@ import User from "../modules/user.js";
 export async function VerifyEmail(req, res) {
   try {
     const { otp, email } = req.body;
-    console.log(email);
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalid email",
+      });
+    }
     // Find the latest OTP entry for the given email
     const getOtp = await Otp.findOne({ email }).sort({ date: -1 });
 
@@ -22,16 +29,36 @@ export async function VerifyEmail(req, res) {
         message: "Invalid OTP. Please check and try again.",
       });
     }
-    const isverify = {
-      isverify: true,
-    };
-    const newdata = await User.updateOne({ email }, isverify);
-    console.log(newdata);
+    // 🟢 Calculate if OTP is expired
+    const now = new Date();
+    const otpCreatedAt = new Date(user.createdAt);
+    const diffMs = now - otpCreatedAt;
+    const diffMins = diffMs / 1000 / 60;
+
+    if (diffMins > 5) {
+      return res.status(400).json({
+        message: "OTP expired. Please request a new one!",
+      });
+    }
+    const updatedUser = await User.findOneAndUpdate(
+      { email: email }, // filter
+      { isverify: true }, // update
+      { new: true } // return updated document
+    );
+
+    const t = await User.findOne({ email });
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
 
     return res.status(200).json({
       message: "Email verified successfully.",
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: "Server error during OTP verification. Please try again.",
     });
