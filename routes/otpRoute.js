90fc988822d3942bc4340f0a920748899987465f
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-07-16 20:29:28 +0530

    add POST /verify and POST / for new OTP requests✅🌟

--- routes/otpRoute.js
+++ routes/otpRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { VerifyEmail } from "../controllers/otpController.js";
+import { requestNewOtp } from "../controllers/userController.js";
 
 const otpRouter = express.Router();
 
-otpRouter.get("/", VerifyEmail);
+otpRouter.post("/verify", VerifyEmail);
+otpRouter.post("/", requestNewOtp);
 
 export default otpRouter;
